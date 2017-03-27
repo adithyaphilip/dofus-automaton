e9412fb5df82fc5a09af7d3bce15cbbe3b9c0aee
Philip <adithyaphilip@gmail.com>
2017-03-27 13:42:01 +0530

    added exception handling and retrying for adb timeouts

--- adb_utils.py
+++ adb_utils.py
@@ -1,5 +1,7 @@
 import subprocess
 import random
+import time
+import sys
 
 _TAP_DURATION_MS = 200
 MOVE_SWIPE_DURATION_MS = 350
@@ -9,11 +11,21 @@ ADB_TIMEOUT_SECONDS = 5
 
 def tap(x: int, y: int):
     duration = random.randint(_TAP_DURATION_MS, _TAP_DURATION_MS + 100)
-    subprocess.call("adb shell input swipe %d %d %d %d %d" % (x, y, x, y, duration), shell=True,
-                    timeout=ADB_TIMEOUT_SECONDS)
+    while True:
+        try:
+            subprocess.call("adb shell input swipe %d %d %d %d %d" % (x, y, x, y, duration), shell=True,
+                            timeout=ADB_TIMEOUT_SECONDS)
+            break
+        except subprocess.TimeoutExpired:
+            print("ADB TIMED OUT!!!! :O :O :O at", time.time(),"- trying again!", file=sys.stderr)
 
 
 def swipe(from_x, from_y, to_x, to_y):
     duration = random.randint(MOVE_SWIPE_DURATION_MS, MOVE_SWIPE_DURATION_MS + 100)
-    subprocess.call("adb shell input swipe %d %d %d %d %d" % (from_x, from_y, to_x, to_y, duration), shell=True,
-                    timeout=ADB_TIMEOUT_SECONDS)
+    while True:
+        try:
+            subprocess.call("adb shell input swipe %d %d %d %d %d" % (from_x, from_y, to_x, to_y, duration), shell=True,
+                            timeout=ADB_TIMEOUT_SECONDS)
+            break
+        except subprocess.TimeoutExpired:
+            print("ADB TIMED OUT!!!! :O :O :O at", time.time(),"- trying again!", file=sys.stderr)
